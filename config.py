e6dcadaae297f98043669c8927e7113f5ed15cdf
Dongun-code <dongun04@outlook.com>
2021-07-02 18:41:49 +0900

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+import os.path as op
+
+class Config:
+    pass
---
+++ model/model_factory.py
@@ -0,0 +1,10 @@
+from model.resnet import ResNet
+
+
+class model_factory:
+    def __init__(self, model):
+        self.model = model
+
+    def get_model(self):
+        if self.model == "renset":
+            return ResNet()
